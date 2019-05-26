c01ddab502b6ca3da51d924f2af384ecdd1c48b2
busti <mbust@live.de>
2019-05-26 21:15:43 +0200

    Type a version of all functions from Iterable for Incremental iterables

--- src/main/scala/polix/collection/RIterable.scala
+++ src/main/scala/polix/collection/RIterable.scala
@@ -1,16 +1,16 @@
-import cats.{Applicative, Monad}
+package polix.collection
 
 import scala.annotation.unchecked.uncheckedVariance
 import scala.language.higherKinds
 
-trait RIterable[+A, R[+_]] extends RIterableOps[A, R, RIterable, RIterable[A, R]]
+trait RIterable[+A, R[+ _]] extends RIterableOps[A, R, RIterable, RIterable[A, R]]
 
 /**
   *
   * @tparam A
   * @tparam R
   */
-trait RIterableOps[+A, R[+_], +CC[_, _[+_]], +C] { this: RIterable[A, R] =>
+trait RIterableOps[+A, R[+ _], +CC[_, _[+ _]], +C] { this: RIterable[A, R] =>
   def collect[B](pf: PartialFunction[A, B]): CC[B, R]
 
   def collectFirst[B](pf: PartialFunction[A, B]): R[Option[B]]
@@ -20,6 +20,8 @@ trait RIterableOps[+A, R[+_], +CC[_, _[+_]], +C] { this: RIterable[A, R] =>
   def count(p: A => Boolean): R[Boolean]
 
   def drop(n: Int): C
+
+  def dropRight(n: Int): C
 
   def dropWhile(p: A => Boolean): C
 
@@ -35,13 +37,35 @@ trait RIterableOps[+A, R[+_], +CC[_, _[+_]], +C] { this: RIterable[A, R] =>
 
   def flatten[B](implicit asRIterable: A => RIterable[B, R]): CC[B, R]
 
-  def foldUndo[B](z: B)(op: (B, A) => B)(undo: (B, A) => B): R[B]
+  def foldUndoLeft[B](z: B)(op: (B, A) => B)(undo: (B, A) => B): R[B]
+
+  def foldUndoRight[B](z: B)(op: (B, A) => B)(undo: (B, A) => B): R[B]
 
   def forall(p: A => Boolean): R[Boolean]
+
+  def groupBy[K](f: A => K): RMap[K, C, R]
+
+  def groupMap[K, B](key: A => K)(f: A => B): RMap[K, CC[B, R], R]
+
+  def groupMapReduceUndo[K, B](key: A => K)(f: A => B)(reduce: (B, B) => B)(
+    undo:                           (B, B) => B
+  ): RMap[K, B, R]
+
+  def grouped = ???
+
+  def head: R[A]
+
+  def headOption: R[Option[A]]
+
+  def init: C
 
   def isEmpty: R[Boolean]
 
   def knownSize: R[Int]
+
+  def last: R[A]
+
+  def lastOption: R[Option[A]]
 
   def map[B](f: A => B): CC[B, R]
 
@@ -55,30 +79,38 @@ trait RIterableOps[+A, R[+_], +CC[_, _[+_]], +C] { this: RIterable[A, R] =>
 
   def nonEmpty: R[Boolean]
 
+  def partition(p: A => Boolean): (C, C)
+
+  def partitionWith[B1, B2](f: A => Either[B1, B2]): (CC[B1, R], CC[B2, R])
+
   def reduceUndo[B >: A](op: (B, B) => B)(undo: (B, B) => B): R[B]
 
   def reduceUndoOption[B >: A](op: (B, B) => B)(undo: (B, B) => B): R[Option[B]]
+
+  def scanUndoLeft[B](z: B)(op: (B, A) => B): CC[B, R]
+
+  def scanUndoRight[B](z: B)(op: (B, A) => B): CC[B, R]
 
   def size: R[Int]
 
   def slice(from: Int, to: Int): C
 
+  def sliding = ???
+
   def span(p: A => Boolean): (C, C)
 
+  def splitAt(n: Int): (C, C)
+
   def sum[B >: A](implicit num: Numeric[B]): R[B] =
-    foldUndo(num.zero)(num.plus)(num.minus)
+    reduceUndo(num.plus)(num.minus)
+
+  def tail: C
 
   def take(n: Int): C
+
+  def takeRight(n: Int): C
 
   def takeWhile(p: A => Boolean): C
 
   def zipWithIndex: CC[(A @uncheckedVariance, Int), R]
-
-  def head: R[A]
-
-  def headOption: R[Option[A]]
-
-  def last: R[A]
-
-  def lastOption: R[Option[A]]
 }

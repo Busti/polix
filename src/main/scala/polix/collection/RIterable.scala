4122c99f208c9815a873e22514dad993daf20cd2
busti <mbust@live.de>
2019-10-03 14:57:54 +0200

    remove type lambda to make streaming type variance possible

--- src/main/scala/polix/collection/RIterable.scala
+++ src/main/scala/polix/collection/RIterable.scala
@@ -5,12 +5,12 @@ import scala.language.reflectiveCalls
 
 import cats.Functor
 
-trait RIterable[+A, +G[_]] extends RIterableOps[A, G, ({ type L[+T] = RIterable[T, G] })#L, RIterable[A, G]] {
+trait RIterable[+A, +G[_]] extends RIterableOps[A, G, RIterable, RIterable[A, G]] {
   def stream: G[E]
 }
 
-sealed trait RIterableOps[+A, +G[_], +CC[_], +C] {
+sealed trait RIterableOps[+A, +G[_], +CC[_, _[_]], +C] {
   type E
 
-  def map[B, J[_] >: G[_] : Functor](f: A => B): CC[B]
+  def map[B, J[_] >: G[_] : Functor](f: A => B): CC[B, J]
 }

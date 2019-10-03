19f97f6ba58fea8ffac45738f3fb515154ca5d8c
busti <mbust@live.de>
2019-10-03 03:23:47 +0200

    subtype the streaming wrapper type +G[_] into J[_] >: G[_] to resolve variance

--- src/main/scala/polix/collection/RIterable.scala
+++ src/main/scala/polix/collection/RIterable.scala
@@ -2,12 +2,12 @@ package polix.collection
 
 import cats.Functor
 
-trait RIterable[+A, +G[_]] extends RIterableOps[A, G, ({ type L[+T] = RIterable[T, G]})#L, RIterable[A, G]] {
+trait RIterable[+A, +G[_]] extends RIterableOps[A, G, ({ type L[+T] = RIterable[T, G] })#L, RIterable[A, G]] {
   def stream: G[E]
 }
 
 sealed trait RIterableOps[+A, +G[_], +CC[_], +C] {
   type E
 
-  def map[B >: A](f: A => B): CC[B]
+  def map[B >: A, J[_] >: G[_] : Functor](f: A => B): CC[B]
 }

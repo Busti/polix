f7797fd3577de37ceb6fd218e8e65ed3801a8d4a
busti <mbust@live.de>
2019-10-04 18:23:27 +0200

    Forward inner type of variant type G2 to G to fix type error in impl

--- src/main/scala/polix/collection/RIterable.scala
+++ src/main/scala/polix/collection/RIterable.scala
@@ -12,5 +12,5 @@ trait RIterable[+A, +G[_]] extends RIterableOps[A, G, RIterable, RIterable[A, G]
 trait RIterableOps[+A, +G[_], +CC[_, _[_]], +C] {
   type E
 
-  def map[B, J[_] >: G[_] : Functor](f: A => B): CC[B, J]
+  def map[B, G2[x] >: G[x] : Functor](f: A => B): CC[B, G2]
 }

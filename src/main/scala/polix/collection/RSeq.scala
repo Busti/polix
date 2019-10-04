22392796af162f8a8f7b73a28f9de293ceddb86b
busti <mbust@live.de>
2019-10-04 18:24:08 +0200

    implement new type structure in RSeq

--- src/main/scala/polix/collection/RSeq.scala
+++ src/main/scala/polix/collection/RSeq.scala
@@ -1,57 +1,31 @@
 package polix.collection
 
-import cats.{Foldable, Functor, Monad}
-import polix.reactive.Scannable
+import cats.Functor
 
-sealed trait RSeqEvent[A]
-case class Insert[A](index: Int, elem: A)                                                       extends RSeqEvent[A]
-case class Remove[A](index: Int)                                                                extends RSeqEvent[A]
-case class Update[A](index: Int, elem: A)                                                       extends RSeqEvent[A]
-case class Combined[A](indexRemoval: Int, indexInsertion: Int, elem: A)                         extends RSeqEvent[A]
-case class Patch[A](index: Int, other: IterableOnce[A], replaced: Int)                          extends RSeqEvent[A]
-case class MassUpdate[A](indicesRemoved: IterableOnce[Int], insertions: IterableOnce[(Int, A)]) extends RSeqEvent[A]
+import scala.language.{higherKinds, reflectiveCalls}
 
-abstract class RSeq[A, G[_]: Monad: Scannable] { self =>
+trait RSeq[A, +G[_]] extends RIterable[A, G] with RSeqOps[A, G, RSeq, RSeq[A, G]] { self =>
+  sealed trait RSeqEvent
+  case class Insert(index: Int, elem: A)                                                       extends RSeqEvent
+  case class Remove(index: Int)                                                                extends RSeqEvent
+  case class Update(index: Int, elem: A)                                                       extends RSeqEvent
+  case class Combined(indexRemoval: Int, indexInsertion: Int, elem: A)                         extends RSeqEvent
+  case class Patch(index: Int, other: IterableOnce[A], replaced: Int)                          extends RSeqEvent
+  case class MassUpdate(indicesRemoved: IterableOnce[Int], insertions: IterableOnce[(Int, A)]) extends RSeqEvent
 
-  def stream: G[RSeqEvent[A]]
+  type E = RSeqEvent
 
-  def materialize: G[Seq[A]] = implicitly[Scannable[G]].scan(self.stream, Seq.empty[A]) {
-    case (acc, Insert(i, e))        => acc.take(i) ++ Seq(e) ++ acc.drop(i) // todo: use `list.splitAt`
-    case (acc, Remove(i))           => acc.take(i) ++ acc.drop(i + 1)
-    case (acc, Update(i, e))        => acc.updated(i, e)
-    case (acc, Combined(ir, ii, e)) => (acc.take(ir) ++ acc.drop(ir + 1)).updated(ii, e)
-    case (acc, Patch(i, o, r))      => acc.patch(i, o, r)
-    case (acc, MassUpdate(ir, in))  =>
-      in.iterator.foldLeft(ir.iterator.foldLeft(acc) { case (a, i) => a.take(i) ++ a.drop(i + 1) }) {
-        case (a, (i, e)) => a.take(i) ++ Seq(e) ++ a.drop(i)
-      }
-  }
-
-  def map[B](f: A => B): RSeq[B, G] = new RSeq[B, G] {
-    override def stream: G[RSeqEvent[B]] = Functor[G].map(self.stream) {
-      case Insert(i, e)        => Insert(i, f(e))
-      case Remove(i)           => Remove(i)
-      case Update(i, e)        => Update(i, f(e))
-      case Combined(ir, ii, e) => Combined(ir, ii, f(e))
-      case Patch(i, o, r)      => Patch(i, o.iterator.map(f), r)
-      case MassUpdate(ir, in)  => MassUpdate(ir, in.iterator.map { case (i, e) => (i, f(e)) })
+  def map[B, J[x] >: G[x] : Functor](f: A => B): RSeq[B, J] = new RSeq[B, J] {
+    override def stream: J[RSeqEvent] = Functor[J].map(self.stream) {
+      case self.Insert(index, elem)                          => Insert(index, f(elem))
+      case self.Remove(index)                                => Remove(index)
+      case self.Update(index, elem)                          => Update(index, f(elem))
+      case self.Combined(indexRemoval, indexInsertion, elem) => Combined(indexRemoval, indexInsertion, f(elem))
+      case self.Patch(index, other, replaced)                => Patch(index, other.iterator.map(f), replaced)
+      case self.MassUpdate(indicesRemoved, insertions) =>
+        MassUpdate(indicesRemoved, insertions.iterator.map { case (i, e) => (i, f(e)) })
     }
   }
-
-  /*def sorted(implicit ord: Ordering[A]): RSeq[A, G] = new RSeq[A, G] {
-    override def stream: G[RSeqEvent] = implicitly[Foldable2[G]].foldLeft(self.stream, Seq.empty[A]) {
-      case (acc, Insert(i, e)) => ???
-      case (acc, Remove(i)) => ???
-      case (acc, Update(i, e)) => ???
-      case (acc, Combined(ir, ii, e)) => ???
-      case (acc, Patch(i, o, r)) => ???
-      case (acc, MassUpdate(ir, in)) => ???
-    }
-  }*/
 }
 
-object RSeq {
-  def from[A, G[_]: Monad: Scannable](initialStream: G[RSeqEvent[A]]): RSeq[A, G] = new RSeq[A, G] {
-    override def stream: G[RSeqEvent[A]] = initialStream
-  }
-}
+trait RSeqOps[+A, +G[_], +CC[_, _[_]], +C] extends RIterableOps[A, G, CC, C]

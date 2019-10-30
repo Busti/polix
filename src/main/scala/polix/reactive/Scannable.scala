5319abbcfe2823258c352c75bcd489e903fbf3cf
busti <mbust@live.de>
2019-10-30 21:17:46 +0100

    refactor scan impl to provide more churrying

--- src/main/scala/polix/reactive/Scannable.scala
+++ src/main/scala/polix/reactive/Scannable.scala
@@ -2,7 +2,7 @@ package polix.reactive
 
 // todo: use simulacrum for typeclasses
 trait Scannable[F[_]] {
-  def scan[A, B](fa: F[A], b: B)(f: (B, A) => B): F[B]
+  def scan[A, B](fa: F[A])(b: B)(f: (B, A) => B): F[B]
 }
 
 object Scannable {

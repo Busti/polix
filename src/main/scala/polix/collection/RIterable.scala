e2a09d7cf608414b163131c1b416622132bc6c3a
busti <mbust@live.de>
2019-10-31 15:48:46 +0100

    add sorted function to RIterable

--- src/main/scala/polix/collection/RIterable.scala
+++ src/main/scala/polix/collection/RIterable.scala
@@ -2,8 +2,8 @@ package polix.collection
 
 import scala.language.higherKinds
 import scala.language.reflectiveCalls
-
 import cats.Functor
+import polix.reactive.Scannable
 
 trait RIterable[+A, +G[_]] extends RIterableOps[A, G, RIterable, RIterable[A, G]] {
   def stream: G[M]
@@ -13,4 +13,6 @@ trait RIterableOps[+A, +G[_], +CC[_, _[_]], +C] {
   type M
 
   def map[B, G2[x] >: G[x] : Functor](f: A => B): CC[B, G2]
+
+  def sorted[A2 >: A, G2[x] >: G[x] : Scannable](implicit ord: Ordering[A2]): CC[A2, G2]
 }

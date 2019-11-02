73138369bc7f116d01a02781afcb8e22e91df73c
busti <mbust@live.de>
2019-11-03 00:02:49 +0100

    fix typo

--- src/main/scala/polix/util/SortedSeqUtils.scala
+++ src/main/scala/polix/util/SortedSeqUtils.scala
@@ -2,7 +2,7 @@ package polix.util
 
 object SortedSeqUtils {
   implicit class SeqOps[T](val wrapped: Seq[T]) extends AnyVal {
-    def sortedInsert(elem: T)(implicit ord2: Ordering[T]): (Seq[T], Int) = {
+    def sortedInsert(elem: T)(implicit ord: Ordering[T]): (Seq[T], Int) = {
       val pos = wrapped.indexWhere(elem => ord2.gt(elem, elem))
       val dst = wrapped.patch(pos, Iterable.single(elem), 0)
       (dst, pos)

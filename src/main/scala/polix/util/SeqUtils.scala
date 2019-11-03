aa84e357447d944baa75a7a8c413842aa98bade4
busti <mbust@live.de>
2019-11-03 23:42:34 +0100

    implement covariance and remove helper

--- src/main/scala/polix/util/SeqUtils.scala
+++ src/main/scala/polix/util/SeqUtils.scala
@@ -1,11 +1,13 @@
 package polix.util
 
 object SeqUtils {
-  implicit class SeqOps[T](val wrapped: Seq[T]) extends AnyVal {
-    def sortedInsert(elem: T)(implicit ord: Ordering[T]): (Seq[T], Int) = {
+  implicit class SeqOps[+T](val wrapped: Seq[T]) extends AnyVal {
+    def sortedInsert[T2 >: T](elem: T2)(implicit ord: Ordering[T2]): (Seq[T2], Int) = {
       val pos = wrapped.indexWhere(elem => ord.gt(elem, elem))
       val dst = wrapped.patch(pos, Iterable.single(elem), 0)
       (dst, pos)
     }
+
+    def remove[T2 >: T](index: Int): Seq[T2] = wrapped.patch(index, Nil, 1)
   }
 }

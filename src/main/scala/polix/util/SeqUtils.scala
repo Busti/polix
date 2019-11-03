c4fe86b1d73d4d121b5f29fd0901829ae6fbf3bc
busti <mbust@live.de>
2019-11-04 00:05:45 +0100

    implement insertion helper

--- src/main/scala/polix/util/SeqUtils.scala
+++ src/main/scala/polix/util/SeqUtils.scala
@@ -2,6 +2,8 @@ package polix.util
 
 object SeqUtils {
   implicit class SeqOps[+T](val wrapped: Seq[T]) extends AnyVal {
+    def insert[T2 >: T](index: Int, elem: T2): Seq[T2] = wrapped.patch(index, Iterable.single(elem), 0)
+
     def sortedInsert[T2 >: T](elem: T2)(implicit ord: Ordering[T2]): (Seq[T2], Int) = {
       val pos = wrapped.indexWhere(elem => ord.gt(elem, elem))
       val dst = wrapped.patch(pos, Iterable.single(elem), 0)

4d7e7c2f73c4663c6b9d46f2b61446b9ffc566b6
busti <mbust@live.de>
2019-11-10 21:17:36 +0100

    create test for RSeq mutations

--- src/test/scala/polix/collection/SpecRSeq.scala
+++ src/test/scala/polix/collection/SpecRSeq.scala
@@ -3,21 +3,36 @@ package polix.collection
 import org.scalatest._
 import org.scalatest.WordSpec
 import org.scalatest.Assertions._
-
+import org.scalatest.Matchers._
 import monix.execution.ExecutionModel.SynchronousExecution
 import monix.execution.Scheduler
 import monix.execution.schedulers.TrampolineScheduler
 import monix.reactive._
 import monix.reactive.subjects._
-import polix.reactive.Scannable
+import polix.collection.RSeqMutations._
 
 class SpecRSeq extends WordSpec {
   implicit val scheduler = TrampolineScheduler(Scheduler.global, SynchronousExecution)
 
-  "something" when {
-    "asdasd" should {
-      "asdasd" in {
-        assert(1 == 1)
+  "RSeq" when {
+    val input: PublishSubject[RSeqMutation[Int]] = PublishSubject()
+    val rseq: RSeq[Observable, Int]              = RSeq.lift(input)
+
+    "mapped" should {
+      val result: RSeq[Observable, Int] = rseq.map(n => n * 2)
+
+      var test: RSeqMutation[Int] = null
+      val output                  = RSeq.drop(result)
+      output.foreach(mutation => test = mutation)
+
+      "map Append" in {
+        input onNext Append(1)
+        test shouldBe Append(2)
+      }
+
+      "map Prepend" in {
+        input onNext Prepend(1)
+        test shouldBe Prepend(2)
       }
     }
   }
